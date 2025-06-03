23c755a0632e6ce6cfbbda6142478f862a1b0081
Okpara Chidimma Phoebe <phoebe.chidimmamars21@gmail.com>
2025-06-03 23:51:35 +0100

    Initial commit

---
+++ file.js
@@ -0,0 +1,38 @@
+//Muiltiplication arithmetic operation
+function muiltNums(num1 = 1, num2 = 2) {
+    return num1 * num2;
+}
+
+console.log(muiltNums(5,5)); 
+
+//Division arithmetic operation
+function divNums () {
+    let num1 = 16;
+    let num2 = 8;
+    return num1 / num2;
+    
+   
+}
+console.log(divNums());
+
+
+//Addition arithmetic operation
+function addition() {
+    let num1 = 46;
+    let num2 = 67;
+    let sum = num1 + num2;
+    return  sum
+    
+}
+console.log(addition());
+
+//Subtraction arithmetic operation
+function subNums () {
+    let num1 = 108;
+    let num2 = 67;
+    let sum = num1 - num2;
+    
+    console.log(sum);
+}
+
+subNums()
