ff2226964ee4683b35b29a475b59ab5f416b8125
Okpara Chidimma Phoebe <phoebe.chidimmamars21@gmail.com>
2025-06-05 11:55:31 +0100

    Checking if the number is positive, negative or zero

---
+++ comparism.js
@@ -0,0 +1,37 @@
+
+// Program to check whether a number is positive, negative, or zero
+
+
+let number = 0;
+
+const check = (number) => {
+
+   if (number > 0){
+    console.log("The number is positive");
+
+   }else if (number < 0){
+    console.log("The number is negative");
+
+   }else {
+    console.log("The number is zero");
+   }
+}
+console.log(check(number));
+
+
+
+
+const nums = 0;
+
+function value(){
+  
+  if (nums > 0){
+    console.log("The number is positive");
+  }else if (nums < 0){
+    console.log("The number is negative");
+  }else{
+    console.log("The number is zero");
+  }
+}
+
+console.log(value(nums));
